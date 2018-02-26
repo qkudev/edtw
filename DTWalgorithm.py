ad91652ca93b994a02678a8d8aeaa02ba759911b
qkudev <qkud.ev@gmail.com>
2018-02-26 08:05:51 +0700

    BACK TO WORK Optimised a little DTW function @TODO:  1) make algorithm for lag like it is in MIDER 	2) make tests!

--- DTWalgorithm.py
+++ DTWalgorithm.py
@@ -1,198 +1,28 @@
-import numpy as np
-
-def divine(X):
-    result_nonnegative = []
-    result_nonpositive = []
-
-    for x in X:
-        if x >= 0:
-            result_nonnegative.append(x)
-        if x <= 0:
-            result_nonpositive.append(x)
-    return [result_nonnegative, result_nonpositive]
-
-def minimum(a, b, c):
-    if (a <= b and a <= c):
-        return -1
-    if (b <=a and b <= c):
-        return 0
-    return 1
-
-def min3(a, b, c):
-    if (a <= b and a <= c):
-        return a
-    if (b <=a and b <= c):
-        return b
-    return c
-
-def average_nozero(X):
-    n = len(X)
-    k = 0
-    sum = 0
-    for i in range(n):
-        sum += X[i]
-        if X[i] == 0:
-            k += 1
-    return sum/(n - k + 1)
-def d(X):
-    n = len(X)
-    result = []
-    for i in range(n - 1):
-        result.append(X[i + 1] - X[i])
-    return result
-
-def normilize(X):
-    minX = min(X)
-    maxX = max(X)
-    d = maxX - minX
-    result = []
-    for i in range(len(X)):
-        result.append((X[i] - minX)/d)
-    return result
-
+from numpy import abs, power, min, zeros, float64, array
+def step(left, left_up, up):
+    if left_up <= left and left_up <= up: return array([1,1])
+    elif left <= left_up and left <= up: return array([1,0])
+    return array([0,1])
 
 def DTW(X,Y, distance):
+    D = zeros((len(X), len(Y)), dtype=float64); p = array([len(X) - 1, len(Y) - 1]); result = []
+    for i in range(len(X)):
+        for j in range(len(Y)):
+            D[i,j] = power(abs(X[i] - Y[j]), distance) + (i==0 and j!=0)*D[0,j-1] + (j==0 and i!=0)*D[i-1,0] + (i!=0 and j!=0)*min([ D[i - 1][j], D[i - 1][j - 1], D[i][j - 1] ] )
 
-    ###
-    ###Variable       | description
-    ###
-    ### X, Y         - input time series of varibles X and Y
-    ### Distance_map - matrix of distances between Xi and Yj
-    ### Moves_map    - matrix of moves
-    ### moves        - 2*m matrix that contains all moves
-    ###                move = -1 is equal to move though Y only
-    ###                move = 0 is equal to move throug X and Y simultanioysly
-    ###                move - 1 is equal to move through X only
-    ###
-    ### result       - resulting 2*m matrix, where  min(length(X) = k, length(Y) = n) <= m <= n + k
-    ### E(X)         - Expected value estimating function
-    ### D(X)         - Despersion estimating function
-
-
-    ###
-    ###Generating distance matrix
-    ###ABSmetric of x and y is |x - y|
-    ###SQRmetric of x and y is |x - y| in the power of k
-    ###
-
-    Distance_map = np.zeros((len(X), len(Y)), np.double)
-    if distance == "abs":
-        for i in range(len(X)):
-            for j in range(len(Y)):
-                    Distance_map[i,j] = np.abs(X[i] - Y[j])
-    else:
-        if distance == "sqr":
-            for i in range(len(X)):
-                for j in range(len(Y)):
-                        Distance_map[i,j] = np.square(X[i] - Y[j])
-        else:
-            for i in range(len(X)):
-                for j in range(len(Y)):
-                    Distance_map[i,j] = np.power(np.abs(X[i] - Y[i]), distance)
-    ###
-    ###Generating moves matrix
-    ###
-
-    Moves_map = np.zeros((len(X), len(Y)), np.double)
-    Moves_map[0][0] = Distance_map[0][0]
-    for i in range(1, len(X)):
-        Moves_map[i][0] = Distance_map[i][0] + Moves_map[i - 1][0]
-
-    for j in range(1, len(Y)):
-        Moves_map[0][j] = Distance_map[0][j] + Moves_map[0][j -1]
-
-    for i in range(1, len(X)):
-        for j in range(1, len(Y)):
-            Moves_map[i][j] = Distance_map[i][j] + min3(Moves_map[i - 1][j], Moves_map[i - 1][j - 1], Moves_map[i][j - 1])
-
-    ####
-    ###Generating result matrix
-    ###
-
-    result = []
-
-    i = len(X) - 1
-    j = len(Y) - 1
-
-    k = 0
-    lag = 0
-    lags = []
-
-    diff = []
-
-    while i != 0 and j != 0:
-        diff.append(i - j)
-        if abs(i - j) > 12:
-            i = i - 1
-            j = j - 1
+    while p[0] != 0 and p[1] != 0:
+        result.append(p)
+        if abs(p[0] - p[1]) > 12:
+            p -= array([1,1])
             continue
-        if i == 0 or j == 0:
-            break
-        result.append([i, j])
-        min = minimum(Moves_map[i - 1][j], Moves_map[i - 1][j - 1], Moves_map[i][j - 1])
-        if min == 0:
-            i -= 1
-            j -= 1
-            if lag != 0:
-                lags.append(k * lag)
-                lag = 0
-            continue
-        if min == -1:
-            i -= 1
-            if lag == -1:
-                k += 1
-            else:
-                if lag == 1:
-                    lags.append(k * lag)
-                k = 1
-                lag = -1
-            continue
-        if lag == 1:
-            k += 1
-        else:
-            if lag == -1:
-                lags.append(k * lag)
-            k = 1
-            lag = 1
-        j -= 1
+        if p[0] == 0 or p[1] == 0: break
+        p -= step(D[p[0] - 1, p[1]], D[p[0] - 1, p[1] - 1], D[p[0], p[1] - 1])
 
-
-    if lag != 0:
-        lags.append(k * lag)
-
-
-    if i == 0 and j != 0:
-        if lag == -1:
-            lags.append(k * lag)
-            k = 1
-        while j != 0:
-            diff.append(i - j)
-            result.append([0, j])
-            k += 1
-            j -= 1
-        lags.append(k)
-
-    if j == 0 and i != 0:
-        if lag == 1:
-            lags.append(k * lag)
-            k = 1
-        while i != 0:
-            diff.append(i - j)
-            result.append([i, 0])
-            k += 1
-            i -= 1
-        lags.append(-k)
-
+    while p[0] != 0 or p[1] != 0:
+        result.append(p)
+        if p[0] != 0: p[0] -= 1
+        else: p[1] -= 1
 
     result.append([0, 0])
-
     result.reverse()
-
-    A = []
-    B = []
-
-    for i in range(len(result)):
-        A.append(result[i][0])
-        B.append(result[i][1])
-
-    return A, B, result, diff
+    return result
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from DTWalgorithm import DTW
-from DTWalgorithm import divine
-import numpy as np
-from matplotlib import pyplot as plt
 import csv
+from matplotlib import pyplot as plt
 from sklearn import metrics
+from numpy import zeros, average, abs, floor, around
 
+def divine(X):
+    result_nonnegative = []
+    result_nonpositive = []
 
+    for x in X:
+        if x >= 0:
+            result_nonnegative.append(x)
+        if x <= 0:
+            result_nonpositive.append(x)
+    return [result_nonnegative, result_nonpositive]
+
+diff = []; TAU = []; VARS = []
 name = "glycolysis"
-
 file = open('datasets/'+ name+'.csv', 'r')
 reader = csv.reader(file)
 for row in reader:
@@ -15,34 +24,20 @@ for row in reader:
     break
 file.close()
 
+LAGS = zeros((N,N))
+for i in range(N): VARS.append([])
+
 tau_file = open("datasets/taumin_" + name + ".csv", 'r')
 file = open('datasets/'+ name+'.csv', 'r')
-
 reader = csv.reader(tau_file)
-
-TAU = []
-LAGS = np.zeros((N,N))
-
-MI_tau = np.zeros((N,N))
-
 
 for row in reader:
     X = [int(k) for k in row]
     TAU.append(X)
 
-for i in range(N):
-    for j in range(N):
-        MI_tau[i][j] = metrics.mutual_info_score()
-
 reader = csv.reader(file)
 
-VARS = []
-
-for i in range(N):
-    VARS.append([])
-
-
-K = np.zeros(5)
+K = zeros(5)
 
 for row in reader:
     for i in range(N):
@@ -52,69 +47,69 @@ for i in range(N):
     for j in range(N):
         X = VARS[i]
         Y = VARS[j]
-        length = len(X)
-        [A, B, result, diff] = DTW(X,Y, "abs")
+        result = DTW(X, Y, 1)
+        for pair in result: diff.append(pair[0] - pair[1])
         diff.sort()
+
         [Eplus, Eminus] = divine(diff)
-        Eplus = np.average(Eplus)
-        Eminus = np.average(Eminus)
-        E = np.average(diff)
+        Eplus = average(Eplus)
+        Eminus = average(Eminus)
+        E = average(diff)
         d = 50
+
         while d > 0.1:
             last = E
             if E >= 0:
-                q = int(np.floor(E))
-                Q = int(len(diff) - 1 - q)
+                q = int(floor(E))
+                Q = len(diff) - 1 - q
                 Iter = diff[:Q]
-                E = np.average(Iter)
+                E = average(Iter)
             else:
-                q = np.floor(np.abs(E))
-                Q = int(len(diff) - q - 1)
+                q = int(floor(-E))
+                Q = len(diff) - q - 1
                 Iter = diff[int(q):]
-                E = np.average(Iter)
-            d = np.abs(last - E)
-        if Eplus <= 1 or Eminus > -1 or abs(abs(Eminus) - Eplus) > 8:
-            E = 0
+                E = average(Iter)
+            d = abs(last - E)
+
+
+        if Eplus <= 1 or Eminus > -1 or abs(Eplus + Eminus) > 8: E = 0
         d = abs(TAU[i][j] - 1 - abs(E))
         for q in range(5):
-            if d < q + 1:
-                K[q] += 1
-        LAGS[i][j] = np.around(E)
+            if d < q + 1: K[q] += 1
 
-        #if np.abs(E - TAU[i][j]) < 5 and TAU[i][j] != 1:
-        #if i == 0 and j == 1:
-        fg = plt.figure()
+        LAGS[i][j] = around(E)
 
-        plt.subplot(211)
-        plt.title("Среднее:" + str(np.average(diff)) + ", Тау: " + str(TAU[i][j] - 1))
-        #ax1 = fg.add_subplot(111)
-        #ax1.text(0.95, 0.9,
-        #         'G'+str(i) + r'$\rightarrow$' + 'G'+str(j) + "\n" +
-        #         #str(i) + r'$\rightarrow$' + str(j) + "\n" +
-        #         #"+E(" + r'$\eta$ )=' + str(Eplus) + "\n" +
-        #         #"-E(" + r'$\eta$ )=' + str(Eminus) + "\n" +
-        #         "E(" + r'$\eta$ )=' + str(E) + "\n" +
-        #         #"D(" + r'$\eta$ )=' + str(np.var(diff)) + "\n" +
-        #         r'$\tau_{min} = $' + str(TAU[i][j] - 1)
-        #         ,
-        #         verticalalignment='top', horizontalalignment='right',
-        #         transform=ax1.transAxes,
-        #         color='black', fontsize=15)
-        plt.hist(diff, bins=30)
+        # #if np.abs(E - TAU[i][j]) < 5 and TAU[i][j] != 1:
+        # #if i == 0 and j == 1:
+        # fg = plt.figure()
+        #
+        # plt.subplot(211)
+        # plt.title("Среднее:" + str(np.average(diff)) + ", Тау: " + str(TAU[i][j] - 1))
+        # #ax1 = fg.add_subplot(111)
+        # #ax1.text(0.95, 0.9,
+        # #         'G'+str(i) + r'$\rightarrow$' + 'G'+str(j) + "\n" +
+        # #         #str(i) + r'$\rightarrow$' + str(j) + "\n" +
+        # #         #"+E(" + r'$\eta$ )=' + str(Eplus) + "\n" +
+        # #         #"-E(" + r'$\eta$ )=' + str(Eminus) + "\n" +
+        # #         "E(" + r'$\eta$ )=' + str(E) + "\n" +
+        # #         #"D(" + r'$\eta$ )=' + str(np.var(diff)) + "\n" +
+        # #         r'$\tau_{min} = $' + str(TAU[i][j] - 1)
+        # #         ,
+        # #         verticalalignment='top', horizontalalignment='right',
+        # #         transform=ax1.transAxes,
+        # #         color='black', fontsize=15)
+        # plt.hist(diff, bins=30)
+        #
+        # #ax2 = fg.add_subplot(121)
+        # plt.subplot(212)
+        # plt.plot(X, linewidth=2.0, label=u'first series')
+        # plt.plot(Y, linewidth=2.0, label=u'second series')
+        #
+        # #ax2.set_color_cycle(['red'])
+        # for k in range(len(result)):
+        #     plt.plot([result[k][0], result[k][1]], [X[result[k][0]], Y[result[k][1]]],color='r', linewidth=0.5)
 
-        #ax2 = fg.add_subplot(121)
-        plt.subplot(212)
-        plt.plot(X, linewidth=2.0, label=u'first series')
-        plt.plot(Y, linewidth=2.0, label=u'second series')
-
-        #ax2.set_color_cycle(['red'])
-        for k in range(len(result)):
-            plt.plot([result[k][0], result[k][1]], [X[result[k][0]], Y[result[k][1]]],color='r', linewidth=0.5)
-
-
-
-for i in range(5):
-    print(str(int(np.floor(K[i]*100/(N*N)))) + '%,\t')
+for i in range(5): print(str(int(floor(K[i]*100/(N*N)))) + '%,\t')
 
 #lagsout = open("Output/lagsout_" + name + ".csv", 'w')
 #Writer = csv.writer(lagsout)
@@ -123,9 +118,7 @@ for i in range(5):
 #    Writer.writerow(row)
 #lagsout.close()
 
-
-
-plt.show()
+#plt.show()
 
 
 
