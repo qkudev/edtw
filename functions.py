faea4e7be98130abac539e782375bcfb4bc1cc19
qkudev <qkud.ev@gmail.com>
2018-03-02 03:24:01 +0700

    =EXTRENNO

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
-from numpy import abs, power, min, zeros, float64, array, sum, log2, mean, logical_and, exp
-from sklearn.metrics import mutual_info_score as I
+from numpy import sum, log2, mean, logical_and, exp
+import numpy as np
+from sklearn.metrics import mutual_info_score as MI
 from time import time
-from numpy import average as E
+
+
+
 
 
 def entropy(X, Y):
@@ -12,74 +15,94 @@ def entropy(X, Y):
     return sum(-p * log2(p) for p in probs)
 
 
-def step(left, left_up, up):
-    if left_up <= left and left_up <= up: return array([1,1])
-    elif left <= left_up and left <= up: return array([1,0])
-
-    return array([0,1])
-
-
 def MI_lag(X, Y, max, T=False):
     ctime = time()
-    min_dist = exp(-I(X, Y))
+    MAX = MI(X, Y)
     lag = 0
 
+    ALL = []
+
     for i in range(1, max):
-        dist_lag = exp(-I(X[i:], Y[:len(Y) - i]))
-        if dist_lag < min_dist:
-            min_dist = dist_lag
+        dist_lag = MI(X[i:], Y[:len(Y) - i])
+        #ALL.append(dist_lag)
+        if dist_lag > MAX:
+            MAX = dist_lag
             lag = i
 
     if T: return [lag, time() - ctime]
+    #return [ALL, lag]
     return lag
 
 
-def DTW(X,Y, distance, lag=False, T=False):
-    ctime = time()
-    D = zeros((len(X), len(Y)), dtype=float64)
-    p = array([len(X) - 1, len(Y) - 1])
-    result = []
-
-    for i in range(len(X)):
-        for j in range(len(Y)):
-            D[i,j] = power(abs(X[i] - Y[j]), distance)
-            D[i,j] += (i==0 and j!=0)*D[0,j-1]
-            D[i,j] += (i!=0 and j==0)*D[i-1,0]
-            D[i,j] += (i!=0 and j!=0)*min([ D[i - 1][j], D[i - 1][j - 1], D[i][j - 1] ] )
-
-
-    while p[0] != 0 and p[1] != 0:
-        result.append([p[0], p[1]])
-        if abs(p[0] - p[1]) > 12:
-            p -= array([1,1])
-            continue
-        if p[0] == 0 or p[1] == 0: break
-        p -= step(D[p[0] - 1, p[1]], D[p[0] - 1, p[1] - 1], D[p[0], p[1] - 1])
-
-
-    while p[0] != 0 or p[1] != 0:
-        result.append([p[0], p[1]])
-        if p[0] != 0: p[0] -= 1
-        else: p[1] -= 1
-
-
-    result.append([0, 0])
-    result.reverse()
-
-    if lag:
-        diffs = []
-        for r in result:    diffs.append(r[0] - r[1])
-        if T: return [E(diffs), time() - ctime]
-        return E(diffs)
-
-    if T: return [result, time() - ctime]
-    return result
-
-
 def path_lag(path):
-    average = 0.
+    lag = 0
+    sum = 0.;
+    k = 0
 
     for p in path:
-        average += p[0] - p[1]
+        difference = abs(p[0] - p[1])
+        if difference == 0: k += 1
+        sum += difference
+        lag = sum/(len(path) - k)
 
-    return average/len(path)
+    sum = 0.;
+    k = 0
+
+    for i in range(int(lag/2), len(path) - int(lag/2)):
+        difference = abs(path[i][0] - path[i][1])
+        if difference == 0: k += 1
+        sum += difference
+        lag = sum / (len(path) -int(lag) -k)
+
+    return lag
+
+    # for p in path:
+    #     sum += p[0] - p[1]
+    # return sum/len(path)
+
+# def DTW(X,Y, distance, lag=False, T=False):
+#     ctime = time()
+#     D = zeros((len(X), len(Y)), dtype=float64)
+#     p = array([len(X) - 1, len(Y) - 1])
+#     result = []
+#
+#     for i in range(len(X)):
+#         for j in range(len(Y)):
+#             D[i,j] = power(abs(X[i] - Y[j]), distance)
+#             D[i,j] += (i==0 and j!=0)*D[0,j-1]
+#             D[i,j] += (i!=0 and j==0)*D[i-1,0]
+#             D[i,j] += (i!=0 and j!=0)*min([ D[i - 1][j], D[i - 1][j - 1], D[i][j - 1] ] )
+#
+#
+#     while p[0] != 0 and p[1] != 0:
+#         result.append([p[0], p[1]])
+#         if abs(p[0] - p[1]) > 12:
+#             p -= array([1,1])
+#             continue
+#         if p[0] == 0 or p[1] == 0: break
+#         p -= step(D[p[0] - 1, p[1]], D[p[0] - 1, p[1] - 1], D[p[0], p[1] - 1])
+#
+#
+#     while p[0] != 0 or p[1] != 0:
+#         result.append([p[0], p[1]])
+#         if p[0] != 0: p[0] -= 1
+#         else: p[1] -= 1
+#
+#
+#     result.append([0, 0])
+#     result.reverse()
+#
+#     if lag:
+#         diffs = []
+#         for r in result:    diffs.append(r[0] - r[1])
+#         if T: return [E(diffs), time() - ctime]
+#         return E(diffs)
+#
+#     if T: return [result, time() - ctime]
+#     return result
+#
+# def step(left, left_up, up):
+#     if left_up <= left and left_up <= up: return array([1,1])
+#     elif left <= left_up and left <= up: return array([1,0])
+#
+#     return array([0,1])
--- tests_weather.py
+++ tests_weather.py
@@ -1,10 +1,21 @@
 import csv
 from datetime import date
 from functions import MI_lag, path_lag
-from numpy import zeros
+from numpy import zeros, array
+from numpy.linalg import norm
 from fastdtw import fastdtw as FDTW
 from scipy.spatial.distance import euclidean
 from time import time
+from sklearn.metrics import mutual_info_score as MI
+import numpy as np
+
+def normilize(X):
+    X = np.array(X)
+    MAX = np.max([ np.max(X), abs(np.min(X)) ])
+    MEAN = np.mean(X)
+    res = []
+    for x in X: res.append((x - MEAN)/MAX)
+    return res
 
 
 ### format: YYYY-MM-DD, average C, average F for 2012-2014 years
@@ -15,6 +26,7 @@ reader = csv.reader(input)
 data = []
 L1 = zeros((3, 3))
 L2 = zeros((3, 3))
+original = zeros((3,3))
 
 for year in range(2012, 2015):
     reader = csv.reader(input)
@@ -29,40 +41,119 @@ for year in range(2012, 2015):
         if date1 <= DATE <= date2:  X.append(AVRG_C)
 
     data.append(X)
-
-##### DATA INPUT END #######
-#######################################
-### MI   ↓
-
-ctime = time()
-
-for i in range(3):
-    for j in range(3):
-        L1[i, j] = MI_lag(data[i][:320], data[j][20:340], 40)
-
-T1 = time() - ctime
-
-### MI   ↑
-#######################################
-### FDTW ↓
-
-ctime = time()
-
-for i in range(3):
-    for j in range(3):
-        L2[ i, j ] = path_lag(FDTW(data[i][:320], data[j][20:340], dist=euclidean)[1])
-
-T2 = time() - ctime
-
-### FDTW ↑
-#######################################
-
-
-print("Lag matrix by Mutual Information")
-print(L1)
-print("elapsed time: {:.2f}s \n".format(T1))
-print("Lag matrix by FDTW")
-print(L2)
-print("elapsed time: {:.2f}s".format(T2))
-
 input.close()
+##### DATA INPUT END #######
+
+
+output = open("out.csv", 'w')
+writer = csv.writer(output)
+X = data[0]
+Y = []; Y.extend(data[1]); Y.extend(data[2])
+X = normilize(X); Y = normilize(Y)
+
+k = 10
+
+while k < 125:
+    row = [k]
+
+    A = X[:365]; B = Y[k:365+k]
+    ctime = time()
+
+    L1 = MI_lag(A,B, 2*k)
+    t1 = time() - ctime
+    err1 = abs(k - L1) / k
+
+    #print("MI lag: {:.2f} \nMI time:{:.2f}s \n".format(L1, time() - ctime))
+
+    ctime = time()
+    [dist, path] = FDTW(A,B, dist=euclidean)
+
+    L2 = path_lag(path)
+    t2 = time() - ctime
+    err2 = abs(k - L2) / k
+    #print("DTW lag: {:.2f} \nDTW time: {:.2f}s \n".format(L2, time() - ctime))
+    row = [k,err1,err2, L1, L2,t1,t2]
+    writer.writerow(row)
+    k += 10
+
+
+
+
+
+# #######################################
+# ### MI   ↓
+#
+# ctime = time()
+#
+# for i in range(3):
+#     for j in range(3):
+#         L1[i, j] = MI_lag(data[i][:240], data[j][100:340], 120)
+#         original[i,j] = 100.
+#
+# T1 = time() - ctime
+#
+# ### MI   ↑
+# #######################################
+# ### FDTW ↓
+#
+# ctime = time()
+#
+# for i in range(3):
+#     for j in range(3):
+#         L2[ i, j ] = path_lag(FDTW(data[i][:240], data[j][100:340], dist=euclidean)[1])
+#
+# T2 = time() - ctime
+#
+# ### FDTW ↑
+# #######################################
+#
+# rel_error1 = norm(L1 - original)/norm(original)
+# rel_error2 = norm(L2 - original)/norm(original)
+#
+# print("Lag matrix by Mutual Information")
+# print(L1)
+# print("elapsed time: {:.2f}s \n".format(T1))
+# print("relative error MI: {}".format(rel_error1))
+#
+# print("-"*50)
+#
+# print("Lag matrix by FDTW")
+# print(L2)
+# print("elapsed time: {:.2f}s".format(T2))
+# print("relative error FDTW: {}".format(rel_error2))
+
+
+#
+# cycle = []
+# for x in data[0]:
+#     cycle.append(x)
+# for y in data[1]:
+#     cycle.append(y)
+#
+# k = 100
+# r = 150
+#
+# X = data[0];
+#
+# Y = cycle[k:k+366]
+#
+# [All, lag] = MI_lag(X,Y, r)
+#
+# [dist, path] = FDTW(X,Y,dist=euclidean)
+#
+# Xaligned = []; Yaligned = []
+# for p in path:
+#     Xaligned.append(X[p[0]])
+#     Yaligned.append(Y[p[1]])
+#
+# [All_aligned, lag_aligned] = MI_lag(Xaligned, Yaligned, r )
+#
+# #print(lag)
+# #print(lag_aligned)
+#
+# for el in All_aligned:
+#     print(el)
+# print(len(All_aligned))
+#
+
+# data[i][:180], data[j][60:240] relErr1 = 0.62, relErr2 = 0.39
