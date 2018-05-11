14b78265ab3b91c9eb2f778856375486bdf55c98
qkudev <qkud.ev@gmail.com>
2018-05-11 10:33:02 +0700

    =Added entropy, joint entropy

--- functions.py
+++ functions.py
@@ -1,6 +1,42 @@
 import numpy as np
-from sklearn.metrics import mutual_info_score as MI
+from sklearn.metrics import mutual_info_score as I
+from scipy import stats
 from numpy import zeros
+
+
+
+def H(X):
+    probs = [np.mean(X == c) for c in set(X)]
+    return np.sum(-p * np.log2(p) for p in probs)
+
+
+def H(X, Y):
+    probs = []
+    for c1 in set(X):
+        for c2 in set(Y):
+            probs.append(np.mean(np.logical_and(X == c1, Y == c2)))
+
+    return np.sum(-p * np.log2(p) for p in probs)
+
+
+def bins(series, resolution = 30):
+    n  = len(series)
+    binned = []
+    for i in range(n // resolution):
+        bin = []
+        for j in range(resolution): bin.append( series[i * resolution + j] )
+        binned.append(bin)
+
+    return binned
+
+
+def d(X,Y):
+    return H(X,Y) - I(X,Y)
+
+
+def D(X,Y):
+    return d(X,Y)/H(X,Y)
+
 
 def normalize(X):
     return np.exp(X)/(np.exp(X) + 1)
@@ -16,36 +52,23 @@ def subsets(X):
     return res
 
 
-def origin(X,Y, lagmax):
-    Imax = 0
-    lag = 0
-    N = len(Y)
-    result = []
-    for i in range(lagmax):
-
-        I = MI(X[lag:], Y[:N - lag])
-        result.append((i, I))
-        if Imax < I:
-            lag = i
-            Imax = I
-    return [result, Imax, lag]
-
-
-def bins(series, resolution = 30):
-
-    n  = len(series)
-    binned = []
-
-    for i in range(n // resolution):
-        bin = []
-        for j in range(resolution): bin.append( series[i * resolution + j] )
-        binned.append(bin)
-
-    return binned
+# def origin(X,Y, lagmax):
+#     Imax = 0
+#     lag = 0
+#     N = len(Y)
+#     result = []
+#     for i in range(lagmax):
+#
+#         I = I(X[lag:], Y[:N - lag])
+#         result.append((i, I))
+#         if Imax < I:
+#             lag = i
+#             Imax = I
+#     return [result, Imax, lag]
 
 
 def EMC(X, Y):
-    return np.exp(-MI(X,Y))
+    return np.exp(-I(X, Y))
 
 def nozero_mean(X):
     sum = 0; count = 1
@@ -55,12 +78,12 @@ def nozero_mean(X):
     return sum/count
 
 def MI_lag(X, Y, max, T=False):
-    MAX = MI(X, Y)
+    MAX = I(X, Y)
     lag = 0
     ALL = [MAX]
 
     for i in range(1, max):
-        I_lagged = MI(X[i:], Y[:len(Y) - i])
+        I_lagged = I(X[i:], Y[:len(Y) - i])
         ALL.append(I_lagged)
         if I_lagged > MAX:
             MAX = I_lagged
